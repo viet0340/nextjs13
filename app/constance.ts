2989b8e6fd4e28980bc68bd6b0778f9df39d185c
vietvv <vietvv_bi@beetsoft.com.vn>
2023-01-06 16:57:31 +0700

    up

---
+++ app/constance.ts
@@ -0,0 +1,37 @@
+type IHeaders = {
+  label: string;
+  value: string;
+};
+
+const headers: IHeaders[] = [
+  {
+    label: 'trang chủ',
+    value: '/',
+  },
+  {
+    label: 'giới thiệu',
+    value: '/gioi-thieu',
+  },
+  {
+    label: 'ván ép',
+    value: '/van-ep',
+  },
+  {
+    label: 'hệ sinh thái fomex',
+    value: '/he-sinh-thai-fomex',
+  },
+  {
+    label: 'thành viên',
+    value: '/thanh-vien',
+  },
+  {
+    label: 'tin tức',
+    value: '/tin-tuc',
+  },
+  {
+    label: 'liên hệ',
+    value: '/lien-he',
+  },
+];
+
+export { headers };
